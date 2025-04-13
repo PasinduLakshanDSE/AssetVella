cfb2f255b057f88e9ac074ea836e25f6b18b14fe
PasinduLakshanDSE <lakshanpasindu749@gmail.com>
2025-04-13 09:00:37 +0530

    coompany dashboard name update

--- assetmanagment/src/components/Company/CompanyDashBoard/CompanyDashBoard.jsx
+++ assetmanagment/src/components/Company/CompanyDashBoard/CompanyDashBoard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import './CompanyDashBoard.css'
+import './companyDashBoard.css'
 import CompanyNavBar from "../CompanyNav/CompanyNav";
 import { Link } from "react-router-dom";
 
